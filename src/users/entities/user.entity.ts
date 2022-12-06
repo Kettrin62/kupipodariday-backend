7f30d4e97edb0b63a697fd08432438064b803772
Kettrin62 <kettykazakova@yandex.ru>
2022-12-06 10:18:31 +0300

    feat: модель Users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import {
+  Length,
+  IsUrl,
+} from 'class-validator';
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({
+    unique: true,
+  })
+  @Length(2, 30)
+  username: string;
+
+  @Column({
+    default: 'Пока ничего не рассказал о себе',
+  })
+  @Length(2, 200)
+  about: string;
+
+  @Column({
+    default: 'https://i.pravatar.cc/300',
+  })
+  @IsUrl()
+  avatar: string;
+
+
+} 
